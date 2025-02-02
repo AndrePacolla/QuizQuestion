1fd615d35ad01be36d8b89632d3031c1ad908edd
AndrePacolla <andreluizzpacolla@outlook.com>
2025-02-02 17:34:35 -0300

    Quiz de Culinária

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+
+
 export default function Home() {
+
+    const title = "Quiz de Culinária";
   return (
-    <>
-     <h1 className="text-red-500">Olá Mundo</h1>
-    
-    </>
+    <div className="w-full h-screen flex justify-center items-center bg-blue-600">
+
+      <div className="w-full max-w-xl rounded-md bg-white text-black shadow shadow-black border-red-400  border-t-4">
+        <div className="p-5 font-bold  text-2xl shadow shadow-gray-950">{title}</div>
+      </div>
+
+    </div>
   );
 }
