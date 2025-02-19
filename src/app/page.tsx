179a63978e068e5c69227340a04d44f69df11fab
AndrePacolla <andreluizzpacolla@outlook.com>
2025-02-19 17:41:45 -0300

    exibir respostas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,29 @@
 "use client"
-
+import { Results } from "@/components/Results";
 import { QuestionItem } from "@/components/QuestionItem";
 import { questions } from "../data/questions";
 import { useState } from "react";
 
   const Page = () => {
 
+  const [answers, setAnswers] = useState<number[]>([]); // historico de respostas
   const [currentQuestion, SetCurrentQuestion] = useState(0); 
+  const [showResult, setShowresult] = useState(false);
+
   const title = "Quiz de Culinária";
 
-  const handleAnswer = (answer: number) => {
+
+  const loadNextQuestion = () => {
+    if(questions[currentQuestion + 1]){
+      SetCurrentQuestion(currentQuestion + 1);
+    }else {
+      setShowresult(true);
+    }
+
+  }
+  const handleAnswer = (answer: number) => {  // lidando com a resposta , seta array clona e passa resposta do parametro
+    setAnswers([... answers, answer]);
+    loadNextQuestion();
 
   }
     
@@ -19,15 +33,32 @@ import { useState } from "react";
       <div className="w-full max-w-xl rounded-md bg-white text-black shadow shadow-black border-red-400  border-t-4">
         <div className="p-5 font-bold  text-2xl border-b border-gray-400">{title}</div>
         <div className="p-5">
-          <QuestionItem 
+          {!showResult &&
+
+           <QuestionItem 
           question = {questions[currentQuestion]}
           count={currentQuestion + 1}
           onAnswer={handleAnswer}
-          />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
-         
+          />   
+          }
+
+          {showResult &&
+
+          <Results questions={questions} answers={answers}/>
+          
+          }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+    
         </div>
         <div className="p-5 text-center border-t border-gray-400 ">
-          {currentQuestion + 1} de {questions.length} pergunta{questions.length === 1 ? "" : "s" }
+          {!showResult &&
+          `${currentQuestion + 1} de ${questions.length} pergunta${questions.length === 1 ? "" : "s" }`
+           }      
+
+           {showResult &&
+            <button className= "px-5 py-3 rounded-md bg-blue-800 text-white font-bold"> Reiniciar Quiz </button>
+           
+           } 
         </div>
       </div>
 
--- src/components/QuestionItem.tsx
+++ src/components/QuestionItem.tsx
@@ -14,15 +14,17 @@ type Props = {
 
     const checkQuestion = (key: number) => {
         if(selectedAnswer === null ){
-            setSelectedAnswer(key);
-
+            setSelectedAnswer(key)
+            
             setTimeout(() => {
                 onAnswer(key);
                 setSelectedAnswer(null);
-            }, 4000);
+            },2000);
         }
     
     }
+
+
 
     return(
         <div>
@@ -34,11 +36,11 @@ type Props = {
                     onClick={() => checkQuestion(key)}
                     className = {`border border-gray-500 mb-2 bg-blue-300 px-3 py-2 rounded-lg 
                            ${selectedAnswer !== null ? "cursor-auto" : "cursor-pointer hover:opacity-60"}
-                           ${selectedAnswer !== null && selectedAnswer === question.answer && selectedAnswer === key  && "bg-green-500 border-green-500"
+                           ${selectedAnswer !== null && selectedAnswer === question.answer && selectedAnswer === key && "bg-green-500 border-green-500"
                            }
                            ${selectedAnswer !== null && selectedAnswer !== question.answer && selectedAnswer === key && "bg-red-700 border-red-950"}
                         `}
-                    >{item} </div>
+                    >{item}</div>
 
                 ))}
             </div>
